75e170833ccd68b2c75c5a56dd609584fc07eed9
Pickra <picker10024@yahoo.com>
2013-09-12 16:42:34 -0400

    added readme file, putzed around w/styles, borrowed mason's mocha test and put in parse sdk

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,9 @@
-console.log('\'Allo \'Allo!');
+Parse.initialize("mnBJxPoO5anLT4b5gtR4oA3dKs0fUdSnCw1TYr6o", "e00VUWvFjONPw4egMhgglRZgpQspAmR8QW7feaZH");
+
+var TestObject = Parse.Object.extend("TestObject");
+var testObject = new TestObject();
+testObject.save({foo: "bar"}, {
+  success: function(object) {
+    alert("yay! it worked");
+  }
+});
--- test/spec/test.js
+++ test/spec/test.js
@@ -1,11 +1,40 @@
 /*global describe, it */
 'use strict';
 (function () {
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
-
-            });
+  describe('The to-do form', function(){
+    this.timeout(15000);
+ 
+    it('should save a new note, said, note should be returned from Parse', function(done){
+      var result;
+ 
+      // $('.form') is refern to the .form class
+      var form = $('.form')
+      // make a random title with which we can query
+      var randomTitle = ''+ Math.floor(Math.random()*10000000)
+      form.find('#title').val(randomTitle)
+      form.find('#content').val('')
+ 
+      // submit it
+      $('.save').click()
+ 
+      setTimeout((function(){
+ 
+        var query = new Parse.Query(NoteClass);
+        query.equalTo("title", randomTitle);
+        query.find({
+          success: function(results) {
+            result = results[0]
+            console.log(results)
+            expect(result.get('title')).to.equal(randomTitle)
+            done()
+          },
+          error: function(error) {
+            done(error.description)
+          }
         });
-    });
+ 
+      }), 2000)
+    }); // end it()
+ 
+  })
 })();
